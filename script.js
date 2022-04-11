0d5505eea1b46391005c1d03ce52089aa7ed4712
Sohaibhasan123 <sohaibcsejust@gmail.com>
2022-04-11 17:56:26 +0600

    problem solve

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ function display(number){
     const result=document.getElementById("result").value;
     document.getElementById("result").value=result+number;
 }
+function clearScreen()
+{
+    document.getElementById("result").value="";
+}
+
+function calculate()
+{
+    const expretion=document.getElementById("result").value;
+    const solution=eval(expretion);
+    document.getElementById("result").value=solution;
+}
