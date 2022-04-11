2caa2f10c16e8499e3e932acce43bf2e544604ba
Sohaibhasan123 <sohaibcsejust@gmail.com>
2022-04-11 18:21:13 +0600

    keyboard functionality adding continues

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ function calculate()
     const solution=eval(expretion);
     document.getElementById("result").value=solution;
 }
+document.addEventListener('keydown', (event)=>
+{
+    if(event.key==1||event.key==2||event.key==3||event.key==4||event.key==5||event.key==6||event.key==7||event.key==8||event.key==9||event.key==0){
+        const result=document.getElementById("result").value;
+        document.getElementById("result").value=result+event.key;
+    }
+    else if(event.keyCode === 13)
+    {
+        calculate();
+    }
+
+});
+
